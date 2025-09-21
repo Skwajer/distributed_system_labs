672b91e97377d90188a140a76de9e7cde8f468de
skwajer <skwajergg@gmail.com>
2025-09-21 23:36:03 +0300

    task 21 is complete, task 22 is in progress

---
+++ src/21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("current proccess id: %d\n", getpid());
+    printf("parent process id: %d\n", getppid());
+    printf("process group id: %d\n", getpgrp());
+    printf("real user id: %d\n", getuid());
+    printf("real calling process group id: %d\n", getgid());
+    printf("effective calling process id: %d\n", geteuid());
+    printf("effective calling process group id: %d\n", getegid());
+
+
+    return 0;
+}
