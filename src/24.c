63586f86afbd14555c175dda07b31b0ef3bc27b1
skwajer <skwajergg@gmail.com>
2025-09-25 17:32:29 +0300

    task 24 is almost ready

--- src/24.c
+++ src/24.c
@@ -5,24 +5,86 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 #define MAX_PATH_LEN 1024
+#define MAX_TYPE_LEN 16
+
+typedef struct dirent dirent;
 
 
 #define ARGC_ERROR -1
 #define FILE_ERROR -2
-#define FORK_ERROR -3
-#define PIPE_ERROR -4
-#define INVALID_ARG -5
-#define FUNC_ERROR -6
+#define PIPE_ERROR -3
+#define INVALID_ARG -4
+#define FUNC_ERROR -5
+#define DIR_ERROR -6
 
 void castom_ls(int recmin, int recmax, char const *curent_path)
 {
-    if (recmin == recmax)
+    DIR *dir;
+    dirent *entry;
+    struct stat statbuf;
+    char *type;
+
+    dir = opendir(curent_path);
+
+    char full_path[MAX_PATH_LEN];
+    if (dir == NULL)
     {
-        printf("|---%s", curent_path);
         return;
     }
+
+    if (recmin == recmax)
+    {
+        printf("|---%s\n", curent_path);
+
+
+        while ((entry = readdir(dir)) != NULL)
+        {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
+            {
+                continue;
+            }
+            snprintf(full_path, sizeof(full_path), "%s/%s", curent_path, entry->d_name);
+            type = strrchr(entry->d_name, '.');
+            type == NULL ?
+                type = "" : ++type;
+                
+            if (stat(full_path, &statbuf) == 0)
+            {
+                if (S_ISREG(statbuf.st_mode)) 
+                {
+                    printf("📄 || name: %s || address: %lu %s\n", entry->d_name, entry->d_ino, type);
+                } 
+                else if (S_ISDIR(statbuf.st_mode)) 
+                {
+                    printf("📁 || name: %s || address: %lu %s\n", entry->d_name, entry->d_ino, type);
+                }
+            }
+        }
+        closedir(dir);
+        return;
+    }
+        while ((entry = readdir(dir)) != NULL) 
+        {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
+            {
+                continue;
+            }
+            
+            char full_path[1024];
+            snprintf(full_path, sizeof(full_path), "%s/%s", curent_path, entry->d_name);
+            
+            struct stat file_stat;
+            if (stat(full_path, &file_stat) == 0 && S_ISDIR(file_stat.st_mode)) 
+            {
+                castom_ls(recmin + 1, recmax, full_path);
+            }
+        }
+    
+    closedir(dir);
 }
 
 int main(int argc, char *argv[])
@@ -46,5 +108,5 @@ int main(int argc, char *argv[])
 
     castom_ls(recmin, recmax, full_path);
 
-
+    return 0;
 }
